85dd482feaf9d27d9c515b448fa5f8e40689fd9e
lazydog616 <xiaozeng@seas.upenn.edu>
2014-02-21 16:35:31 -0500

    scene set up code finished, need to merge code with other teammates

---
+++ Assets/Source/Constants.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Constants {
+	//game implementation
+	public const int player_life_amount = 3;
+	public const int level_time_limit = 90;//90 sec
+	public static Vector3 player_start_pos = new Vector3 (0, 0, 0);
+
+	//scoring  regularion
+	public const int life_bonus = 200;
+	public const int reaching_destination_score = 500;
+
+}
---
+++ Assets/Source/GameState.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameState : MonoBehaviour {
+
+	public string player_name;
+	public float score;
+	public int remaining_life_amount;
+	public float timer;
+	private GUIText gameplay_info;
+
+	// Use this for initialization
+	void Start () {
+		player_name = "test_player";
+		score = 0;
+		remaining_life_amount = Constants.player_life_amount;
+		timer = Constants.level_time_limit;
+		gameplay_info = GameObject.Find ("GamePlayInfo").GetComponent<GUIText> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//game play info gui
+		gameplay_info.text = "Player life : " + remaining_life_amount + "   Score : " + score + "  Timer : " + (int)timer;
+
+		//time code
+		timer -= Time.deltaTime;
+		//TODO: change to right level name
+		if (timer <= 0) {
+			if(remaining_life_amount > 0)
+			{
+				//time limit passed, move player back to the start pos, and decrease player life amount
+				//reset timer
+				GameObject player = GameObject.FindGameObjectWithTag("Player");
+				player.transform.position = Constants.player_start_pos;
+				remaining_life_amount -= 1;
+				timer = Constants.level_time_limit;
+			}
+			else{
+				PlayerPrefs.SetFloat(player_name, score);
+				Application.LoadLevel ("Score_Scene");
+			}
+		}
+	
+	}
+}
