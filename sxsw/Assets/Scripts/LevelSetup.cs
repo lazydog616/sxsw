34126d9a643f9d44d1b2c387e749eafa908d7a42
unknown <winifred-shao@hotmail.com>
2014-02-26 11:30:24 -0500

    plugins working

---
+++ sxsw/Assets/Scripts/LevelSetup.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+
+
+public class LevelSetup : MonoBehaviour {
+
+	float maxRadius = 40;
+	int threshold = 80; //range 0-255
+	float[] objPosX;
+	float[] objPosY;
+	float[] objHeight;
+	float[] objWidth;
+	
+	[DllImport("ShapeDetectionUnity")]
+	public static extern int detectShape(float maxRadius, int threshold, float[] objPosX, float[] objPosY, float[] objHeight, float[] objWidth); 
+
+	// Use this for initialization
+	void Start () {
+		objPosX = new float[100];
+		objPosY = new float[100];
+		objHeight = new float[100];
+		objWidth = new float[100];
+		
+		for(int i = 0; i < 100; i++)
+		{
+			objPosX[i] = objPosY[i] = objHeight[i] = objWidth[i] = 0;
+		}
+		int sizeNum = detectShape(maxRadius, threshold, objPosX, objPosY, objHeight, objWidth);
+		
+		print(sizeNum);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ sxsw/Assets/Scripts/ResultGlobal.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResultGlobal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log (PlayerPrefs.GetString ("result"));
+		if (PlayerPrefs.GetString ("result") == "win") {
+			GameObject.Find("result").guiText.text = "YOU WIN";
+		}
+		else
+		{
+			GameObject.Find("result").guiText.text = "You LOSE!";
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
