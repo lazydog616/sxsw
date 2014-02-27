98f99bbd49d76f7ed881fdc1b9e65c73cad15f9f
lazydog616 <xiaozeng@seas.upenn.edu>
2014-02-27 17:32:34 -0500

    add spring and gear rotate

--- sxsw/Assets/Scripts/Collisitions.cs
+++ sxsw/Assets/Scripts/Collisitions.cs
@@ -10,29 +10,42 @@ public class Collisitions : MonoBehaviour {
 		PlayerPrefs.SetString ("result", "lose");
 		positionori = gameObject.transform.position;
 	}
-	
+	bool get_springed = false;
+	Vector3 spring_up_velocity;
 	// Update is called once per frame
 	void Update () {
-	
+		
+	}
+	void FixedUpdate(){
+		if (get_springed) {
+			//Debug.Log("hello");
+			gameObject.transform.position += spring_up_velocity * Time.deltaTime;
+			spring_up_velocity -= Vector3.up * 9.8f * Time.deltaTime;
+			if(spring_up_velocity.y < 100.0f) get_springed = false;
+
+			//get_springed = false;
+		}
+
 	}
 	void OnTriggerEnter(Collider collider)
 	{
 		Debug.Log (collider.name);
-		if (collider.tag == "fire" || collider.tag == "water" ||collider.tag == "gear") 
-		{
-			life --;
-			if (life > 0)
-					reSet ();
-			else
-			{
-				Destroy (gameObject);
-				Application.LoadLevel(1);
-			}
-		} 
-		else if (collider.tag == "door") {
-			PlayerPrefs.SetString("result","win");
-			Application.LoadLevel(1);			
-		}
+		if (collider.tag == "fire" || collider.tag == "water" || collider.tag == "gear") {
+						life --;
+						if (life > 0)
+								reSet ();
+						else {
+								Destroy (gameObject);
+								Application.LoadLevel (1);
+						}
+				} else if (collider.tag == "door") {
+						PlayerPrefs.SetString ("result", "win");
+						Application.LoadLevel (1);			
+				} else if (collider.tag == "spring") {
+						Debug.Log("spring!!");
+						get_springed = true;
+						spring_up_velocity = Vector3.up * 200;
+				}
 	}
 	void reSet()
 	{
